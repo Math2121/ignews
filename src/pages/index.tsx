f3c7b1d199ae4df63e8b6d8a51474b4245622c29
Math2121 <matheusdepaula527@gmail.com>
2021-05-31 15:49:52 -0300

    intro page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import style from "./home.module.scss";
 
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 export default function Home() {
   return (
     <>
@@ -12,12 +13,13 @@ export default function Home() {
           <span>👏 Hey, Welcome</span>
 
           <h1>
-            News about the <span>React</span> world.
-            <p>
-              Get access to all the publictions <br />
-              <span>for $9.90 month</span>
-            </p>
+            News about the <span>React</span> world.{" "}
           </h1>
+          <p>
+            Get access to all the publictions <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton/>
         </section>
 
         <img src="/images/avatar.svg" alt=" Girl Coding" />
