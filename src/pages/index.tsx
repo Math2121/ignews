755af01d1c7d8d60012fb5c99cbb50b2a7792389
Math2121 <matheusdepaula527@gmail.com>
2021-06-02 18:31:02 -0300

    adicionando SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import style from "./home.module.scss";
 
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { stripe } from "../services/stripe";
 
 interface HomeProps {
@@ -38,7 +38,7 @@ export default function Home({product}:HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1IxdFZJCB9W3nhHIsl4nHIJ1", {
     expand: ["product"],
   });
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
      product
     },
+    revalidate: 60 * 60 * 24,// 24 horas
   };
 };
