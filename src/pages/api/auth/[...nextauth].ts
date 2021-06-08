e141f6504f6fd0eeaedf7d9ceb0ddfa407c720a3
Math2121 <matheusdepaula527@gmail.com>
2021-06-08 16:13:48 -0300

    connect and create FaunaDB database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,50 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
-
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { query as query } from "faunadb";
+import { fauna } from "../../../services/fauna";
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope:'read:user'
+      scope: "read:user",
     }),
   ],
-})
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        //VErifica se existe um usuário com determinado e-mail no banco
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index("user_by_email"),
+                  query.Casefold(email)
+                )
+              )
+            ),//else
+            query.Create(
+              query.Collection("users"), 
+              { data: { email } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index("user_by_email"),
+                query.Casefold(email)
+              )
+            )
+          )
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,6 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       product,
-  
     },
     revalidate: 60 * 60 * 24, // 24 horas
   };
